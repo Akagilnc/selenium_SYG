9f64b1da8e21235410c0b8205d3f333853278cb3
DeronLee <akagilnc@gmail.com>
2017-08-10 21:08:06 +0800

    add some assert and refactoror test.py add class and method

--- test.py
+++ test.py
@@ -1,31 +1,44 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-## System.setProperty("webdriver.gecko.driver", "/Users/hectester/Documents");
-## 创建浏览器对象
-browser = webdriver.Chrome()
-browser.implicitly_wait(10)
-browser.set_window_size(1680, 1080)
-## 打开SYG页面
-browser.get('http://10.10.10.54:2334/product/SKII-0066')
-time.sleep(4)
-login_elem = browser.find_element_by_xpath('/html/body/div/header/div/div[2]/a[1]/span')
+class TryLoginAndBackToProductPage(unittest.TestCase):
+    def setUp(self):
+        ## 创建浏览器对象
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
+        self.browser.set_window_size(1680, 1080)
 
-login_elem.click()
+    def test_process_in_SYG(self):
+        browser = self.browser
+        ## 打开SYG页面
+        browser.get('http://10.10.10.54:2334/product/SKII-0066')
+        time.sleep(4)
+        assert "登陆" in browser.page_source
 
-account_elem = browser.find_element_by_id('account')
-password_elem = browser.find_element_by_id('password')
+        login_elem = browser.find_element_by_xpath('/html/body/div/header/div/div[2]/a[1]/span')
+        login_elem.click()
+        time.sleep(4)
+        assert '登陆' in browser.title
 
-account_elem.send_keys('13800001111')
-password_elem.send_keys('a123456')
-account_elem.click()
+        account_elem = browser.find_element_by_id('account')
+        password_elem = browser.find_element_by_id('password')
+        account_elem.clear()
+        account_elem.send_keys('13800001111')
+        password_elem.send_keys('a123456')
+        account_elem.click()
+        time.sleep(0.5)
+        login_btn = browser.find_element_by_id('submit_btn')
+        login_btn.click()
+        time.sleep(4)
+        assert "商品" in browser.title
 
-time.sleep(0.5)
+    def setDown(self):
+        time.sleep(10)
+        self.browser.quit()
 
-login_btn = browser.find_element_by_id('submit_btn')
-login_btn.click()
 
-time.sleep(10)
-browser.quit()
+if __name__ == '__main__':
+    unittest.main()
