6df1a974e36fcbceb883c3aed1d7cd7c3e7b0b52
Deron Lee <akagilnc@gmail.com>
2017-08-11 17:38:42 +0800

    add BrowserStack support

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 
 
 class TryLoginAndBackToProductPage(unittest.TestCase):
     def setUp(self):
         ## 创建浏览器对象
-        self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(10)
-        self.browser.set_window_size(1680, 1080)
+        desired_cap = {'browser': 'chrome', 'build': 'First build', 'browserstack.debug': 'true', 'browserstack.local': 'true'}
+
+        self.driver = webdriver.Remote(
+            command_executor='http://deronlee1:Ayr2VfvqYzkirpAfYt8t@hub.browserstack.com:80/wd/hub',
+            desired_capabilities=desired_cap)
 
     def test_process_in_SYG(self):
-        browser = self.browser
+        browser = self.driver
         ## 打开SYG页面
         browser.get('http://10.10.10.54:2334/product/SKII-0066')
         time.sleep(4)
-        assert 'href="/login"' in browser.page_source
+        # assert 'href="/login"' in browser.page_source
 
         login_elem = browser.find_element_by_xpath('/html/body/div/header/div/div[2]/a[1]/span')
         login_elem.click()
